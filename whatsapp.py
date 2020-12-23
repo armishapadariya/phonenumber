97be0cfebfbae79b664af68cbebfcad56a239e3b
armishapadariya <66558691+armishapadariya@users.noreply.github.com>
2020-12-23 20:53:38 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,4 +1,4 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+918460475226', "hello jenu",20,46)
+pywhatkit.sendwhatmsg('+91 mob.no', "hello jenu",20,46)
 
 pywhatkit.text_to_handwriting("i m armisha",rgb=[0,0,0])
